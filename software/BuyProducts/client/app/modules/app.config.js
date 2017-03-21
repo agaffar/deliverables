fa6de34699bffd04dc37a26950646519074abaea
agaffar <bismillah@213>
2017-03-21 15:07:02 +0530

    BuyProducts task initial commit

---
+++ software/BuyProducts/client/app/modules/app.config.js
@@ -0,0 +1,38 @@
+/**
+ * Created by SB004 on 3/21/2017.
+ */
+/**
+ * Created by SB004 on 3/20/2017.
+ */
+(function(){
+    angular.module('sumCamp').config(navConfig);
+    navConfig.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+    function navConfig($stateProvider, $urlRouterProvider, $locationProvider)
+    {
+        //console.log("uuuuuuuuuuu");
+        $stateProvider.state('home', {
+            url:"/home",
+            controller: 'homeController',
+            controllerAs: 'hc',
+            templateUrl: '/partials/homePage.html'
+
+        });
+        $stateProvider.state('addCourse', {
+            url:"/addCourse",
+            controller: 'addCourseController',
+            controllerAs: 'acc',
+            templateUrl: '/partials/addCourse.html'
+
+        });
+        $stateProvider.state('enrollStudent', {
+            url:"/enrollStudent/:courseId",
+            controller: 'enrollController',
+            controllerAs: 'ec',
+            templateUrl: '/partials/enrollStudent.html'
+
+        });
+        $urlRouterProvider.otherwise('home');
+        //$locationProvider.html5Mode(true);
+    }
+})();
+
---
+++ software/BuyProducts/client/app/modules/app.module.js
@@ -0,0 +1,10 @@
+/**
+ * Created by SB004 on 3/21/2017.
+ */
+(function(){
+'use strict'
+    angular.module('buyProd',[ 'ui.router',
+        'ui.bootstrap',
+        'ngTable',
+        'ngResource'])
+})();
